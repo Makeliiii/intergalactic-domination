119382ace8b2d1906f660d56661454392f600d6a
Joel <joel.m.maenpaa@gmail.com>
2019-10-08 22:27:40 +0300

    added verify and logout route, dunno if anything works yet

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -32,18 +32,68 @@ router.route('/login').post((req, res) => {
             return console.log(err)
         }
         if (users.length != 1) {
-            return console.log(err)
+            return res.send({
+                success: false
+            })
         }
 
         const user = users[0]
         if (!user.comparePasswords(password)) {
-            return console.log(err)
+            return res.send({
+                success: false
+            })
         }
 
         newUserSession.userId = user._id
         newUserSession.save()
             .then(() => res.json('User session added'))
             .catch(err => res.status(400).json('Error: ' + err))
+    })
+})
+
+router.route('/verify').get((req, res) => {
+    const { query } = req,
+          { token } = query
+    
+    UserSession.find({
+        _id: token,
+        isDeleted: false
+    }, (err, sessions) => {
+        if (err) {
+            return console.log(err)
+        }
+
+        if (sessions.length != 1) {
+            return res.send({
+                success: false
+            })
+        } else {
+            return res.send({
+                success: true
+            })
+        }
+    })
+})
+
+router.route('/logout').get((req, res) => {
+    const { query } = req,
+          { token } = query
+
+    UserSession.findOneAndUpdate({
+        _id: token,
+        isDeleted: false
+    }, {
+        $set: {
+            isDeleted: true
+        }
+    }, null, (err, sessions) => {
+        if (err) {
+            console.log(err)
+        }
+
+        return res.send({
+            success: true
+        })
     })
 })
 
